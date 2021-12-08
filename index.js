9329ab019a57021b918dd8b9e78cbfa3d4cb99ea
dankrzeminski32 <dankrzeminski32@gmail.com>
2021-12-08 10:23:44 -0600

    Weather Widget for one location complete

---
+++ index.js
@@ -0,0 +1,59 @@
+fetchWeather();
+
+function showWeatherItems(data) {
+  let row = document.querySelector(".three-day-forecast");
+  console.log(data.daily.toString());
+  data.daily = data.daily.splice(1);
+  console.log(data.daily);
+  row.innerHTML = data.daily
+    .map((day, idx) => {
+      if (idx < 3) {
+        let dt = new Date(day.dt * 1000);
+        return `
+          <li class="first">
+            <p class="day">${dt.toDateString().slice(0, 3)}</p>
+            <img src="http://openweathermap.org/img/wn/${
+              day.weather[0].icon
+            }@4x.png" class="weth-logo" />
+            <p class="temp">${Math.trunc(day.temp.day)}&#8457</p>
+          </li>
+    `;
+      }
+    })
+    .join(" ");
+}
+
+function showWeatherDescription(data) {
+  let row = document.querySelector(".weather-description");
+  console.log(data.current.weather[0].description);
+  row.innerHTML = `${data.current.weather[0].description}`;
+}
+
+function showWeatherHeader(data) {
+  let row = document.querySelector(".container-current");
+  row.innerHTML = `<img class="current weth-logo" src="http://openweathermap.org/img/wn/${
+    data.current.weather[0].icon
+  }@4x.png" alt="" /><h1 class="current-temp">${Math.trunc(
+    data.current.temp
+  )}&#8457</h1> `;
+}
+
+function fetchWeather() {
+  let key = "***REMOVED***";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=39.7684&lon=-86.1581&exclude=minutely,alerts&appid=${key}&units=imperial`;
+  //fetch the API
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      showWeatherItems(data);
+      showWeatherHeader(data);
+      showWeatherDescription(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
