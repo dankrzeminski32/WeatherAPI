d16abc892ff34b0ce789e81911fe0f514eb77889
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-01-01 23:08:10 -0600

    security

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function showWeatherHeader(data) {
 }
 
 function fetchWeather() {
-  let key = "***REMOVED***";
+  let key = config.MY_KEY;
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=39.7684&lon=-86.1581&exclude=minutely,alerts&appid=${key}&units=imperial`;
   //fetch the API
   fetch(url)
